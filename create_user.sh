6065ba652442e6d1f5920ec45adeebcd90c713f2
Christopher Price <119444515+chrislprice@users.noreply.github.com>
2022-12-09 23:56:17 -0500

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #Create a new user or not if it already exists
 
-echo "Hello! Please enter your name to verify if you need an account created or one is already setup for you:"
+echo "Hello! Please enter your name to verify if you need an account created or if it already exists:"
 read username
 echo "Please standy by while we check your status"
 
 if getent passwd "$username" > /dev/null 2>&1;
  then
-	echo "Username already exists within our database"
+        echo "Username already exists within our system. Please login with your credentials"
 else
-	echo "It appears we need to create the account, enter password to create the account."
-	sudo useradd -m $username
-	echo "Account created!"
+        echo "It appears we need to create the account. Default password will be username@linux"
+        sudo useradd -m $username
+        password="$username@linux"
+        echo -e "$password\n$password\n" | sudo passwd $username
+        echo "Account created!"
 fi
